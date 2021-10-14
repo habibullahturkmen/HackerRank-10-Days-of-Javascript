2cb496d55629deb9a723571dcc8498885cb63988
Habibullah Turkmen <habibullah_turkmen@yahoo.com>
2021-10-14 19:39:58 +0430

    Update Day5ArrowFunctions.js

--- Day5ArrowFunctions.js
+++ Day5ArrowFunctions.js
@@ -9,10 +9,8 @@ function modifyArray(nums) {
     return nums.map(n => (n % 2 === 0) ? n * 2 : n * 3);
 }
 
-
 function main() {
     const a = [1, 2, 3, 4, 5];
-
     console.log(modifyArray(a).toString().split(',').join(' '));
 }
 
